7f6b8fa925469a95e1607f0c6da2d90c02dd390e
JoJaCo <contrerasjorge226@gmail.com>
2020-11-14 00:02:25 -0800

    adding cal app

--- app/src/main/java/com/calivera/andriod/mycalculator/MainActivity.kt
+++ app/src/main/java/com/calivera/andriod/mycalculator/MainActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvInput
+
     }
 
     fun onDigit(view: View){
