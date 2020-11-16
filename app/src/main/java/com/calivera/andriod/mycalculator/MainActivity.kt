9d746bbbba152abf9c2c08d0f3ebc06be60551b1
JoJaCo <contrerasjorge226@gmail.com>
2020-11-15 21:02:07 -0800

    readded the functionality to the buttons and removed the zero from the end of the numbers

--- app/src/main/java/com/calivera/andriod/mycalculator/MainActivity.kt
+++ app/src/main/java/com/calivera/andriod/mycalculator/MainActivity.kt
@@ -48,20 +48,73 @@ class MainActivity : AppCompatActivity() {
     fun onEqual(view: View){
         if (lastNumeric){
             var tvValue = tvInput.text.toString()
+            var prefix = ""
 
             try {
+                if (tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue= tvValue.substring(1)
+                }
+                //subtract
                 if(tvValue.contains("-")){
                         val splitValue = tvValue.split("-")
                         var one = splitValue[0]
                         var two = splitValue[1]
 
-                        tvInput.text = (one.toDouble() - two.toDouble()).toString()
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                        tvInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                    //divide
+                }else if(tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                    //Addition
+                }else if(tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                    //Multiply
+                }else if(tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
                 }
 
             }catch (e: ArithmeticException) {
                 e.printStackTrace()
             }
         }
+    }
+
+    private fun removeZeroAfterDot(result: String): String{
+        var value = result
+        if(result.contains(".0")){
+            value = result.substring(0,result.length - 2)
+
+
+        }
+        return value
     }
 
 
